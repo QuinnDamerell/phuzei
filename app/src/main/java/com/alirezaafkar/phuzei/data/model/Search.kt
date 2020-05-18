6677171c297eaa61c37f586585282add61668244
Quinn Damerell <qdamere@microsoft.com>
2020-05-18 09:36:55 -0700

    Forcing all calls to only invoke favorite pictures and increasing the page size to 100 so the shuffle has more to randomize.

--- app/src/main/java/com/alirezaafkar/phuzei/data/model/Search.kt
+++ app/src/main/java/com/alirezaafkar/phuzei/data/model/Search.kt
@@ -5,8 +5,15 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by Alireza Afkar on 14/9/2018AD.
  */
+data class FeatureFilter(
+        @SerializedName("includedFeatures") val includedFeatures: List<String>
+)
+data class Filter(
+        @SerializedName("featureFilter") val featureFilter: FeatureFilter
+        )
+
 data class Search(
-    @SerializedName("albumId") val albumId: String,
-    @SerializedName("pageSize") val pageSize: Int = 20,
+        @SerializedName("filters") val filters: Filter,
+    @SerializedName("pageSize") val pageSize: Int = 100,
     @SerializedName("pageToken") val pageToken: String? = null
 )
--- app/src/main/java/com/alirezaafkar/phuzei/data/repository/PhotosRepository.kt
+++ app/src/main/java/com/alirezaafkar/phuzei/data/repository/PhotosRepository.kt
@@ -1,6 +1,8 @@
 package com.alirezaafkar.phuzei.data.repository
 
 import com.alirezaafkar.phuzei.data.api.PhotosApi
+import com.alirezaafkar.phuzei.data.model.FeatureFilter
+import com.alirezaafkar.phuzei.data.model.Filter
 import com.alirezaafkar.phuzei.data.model.Search
 import com.alirezaafkar.phuzei.data.model.SearchResponse
 import com.alirezaafkar.phuzei.util.applyNetworkSchedulers
@@ -12,12 +14,14 @@ import javax.inject.Inject
  */
 class PhotosRepository @Inject constructor(private var api: PhotosApi) {
     fun getAlbumPhotos(albumId: String, pageToken: String? = null): Single<SearchResponse> {
-        val search = Search(albumId = albumId, pageToken = pageToken)
+        val fea = FeatureFilter(includedFeatures = listOf("FAVORITES"))
+        val search = Search(pageToken = pageToken, filters = Filter(featureFilter =  fea))
         return api.getAlbumPhotos(search).compose(applyNetworkSchedulers())
     }
 
     fun getAlbumPhotosSync(albumId: String, pageToken: String? = null): SearchResponse? {
-        val search = Search(albumId = albumId, pageToken = pageToken)
+        val fea = FeatureFilter(includedFeatures = listOf("FAVORITES"))
+        val search = Search(pageToken = pageToken, filters = Filter(featureFilter =  fea))
         return api.getAlbumPhotosCall(search).execute().body()
     }
 
